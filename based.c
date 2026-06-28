f454c5d4cbff20402cbca06280d9d152ee4b21a1
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2026-06-28 10:17:13 +0100

    add base64 encode function with padding

--- based.c
+++ based.c
@@ -1,5 +1,8 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const char base64_table[] = 
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -7,32 +10,50 @@ const char base64_table[] =
     "0123456789"
     "+/";
 
-int main() {
-    char *input = "ABC";
-    size_t input_len = 3;
+char *base64_encode(const char *input, size_t len) {
+    size_t out_len = 4 * ((len + 2) / 3);
+    char *out = malloc(out_len + 1);
+    size_t out_i = 0;
 
-    printf("'%s' in binary:\n", input);
-    for (size_t i = 0; i < input_len; i++) {
-        printf("%08b ", input[i]);
+    char prev = 0;
+    for (size_t i = 0; i < len; i++) {
+        char curr = input[i];
+        switch (out_i % 4) {
+        case 0:
+            out[out_i++] = base64_table[curr >> 2];
+            break;
+        case 1:
+            out[out_i++] = base64_table[((prev & 0x03) << 4) + (curr >> 4)];
+            break;
+        case 2:
+            out[out_i++] = base64_table[((prev & 0x0f) << 2) + (curr >> 6)];
+            out[out_i++] = base64_table[curr & 0x3f];
+            break;
+        }
+        prev = curr;
     }
-    printf("\n\n");
-    // 01000001 01000010 01000011
 
-    printf("6-bit transformation:\n");
-    printf("%06b ", input[0] >> 2);
-    printf("%06b ", ((input[0] & 0x03) << 4) + (input[1] >> 4));
-    printf("%06b ", ((input[1] & 0x0f) << 2) + (input[2] >> 6));
-    printf("%06b ", input[2] & 0x3f);
-    printf("\n\n");
-    // 010000 010100 001001 000011
+    switch (out_i % 4) {
+    case 1:
+        out[out_i++] = base64_table[((prev & 0x03) << 4)];
+        out[out_i++] = '=';
+        out[out_i++] = '=';
+        break;
+    case 2:
+        out[out_i++] = base64_table[((prev & 0x0f) << 2)];
+        out[out_i++] = '=';
+        break;
+    }
 
-    printf("Base64 encoded: \n");
-    printf("%c", base64_table[input[0] >> 2]);
-    printf("%c", base64_table[((input[0] & 0x03) << 4) + (input[1] >> 4)]);
-    printf("%c", base64_table[((input[1] & 0x0f) << 2) + (input[2] >> 6)]);
-    printf("%c", base64_table[input[2] & 0x3f]);
-    printf("\n");
-    // QUJD
-    //
-    // Same output as `echo -n "ABC" | base64`!
+    out[out_i] = 0;
+
+    return out;
+}
+
+int main() {
+    char *input = "Hi";
+    printf("Input:\n%s\n", input);
+
+    char *encoded = base64_encode(input, strlen(input));
+    printf("Encoded:\n%s\n", encoded);
 }
